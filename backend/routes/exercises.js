44d847c42ffa6210f330ac0cbbd88fbe78a3f49e
“Rohit <rohitkushwaha798222@gmail.com>
2024-07-27 23:00:28 +0530

    added delete, update, search by id - used postman

---
+++ backend/routes/exercises.js
@@ -0,0 +1,54 @@
+const router = require("express").Router();
+let Exercise = require('../models/exercise.model');
+
+// Get all
+router.route('/').get((req,res)=>{
+    Exercise.find()
+    .then(exercise => res.json(exercise))
+    .catch(err => res.status(400).json('Error'+err));
+});
+
+// Add exercise
+router.route('/add').post((req,res)=>{
+    const username = req.body.username;
+    const description = req.body.description;
+    const duration = Number(req.body.duration);
+    const date = Date.parse(req.body.date);
+    const newExercise = new Exercise({username,description,duration,date});
+        
+    newExercise.save()
+    .then(() => res.json('Exercise Added'))
+    .catch(err => res.status(400).json('Error'+err));
+});
+
+// Get by id individual
+router.route('/:id').get((req,res)=>{
+    Exercise.findById(req.params.id)
+    .then(exercise => res.json(exercise))
+    .catch(err => res.status(400).json('Error'+err));
+});
+
+// Delete by ID
+router.route('/del/:id').delete((req,res)=>{
+    Exercise.findByIdAndDelete(req.params.id)
+    .then(exercise => res.json("Exercise Deleted!"))
+    .catch(err => res.status(400).json('Error'+err));
+});
+
+// Update by ID
+router.route('/update/:id').post((req,res)=>{
+    Exercise.findById(req.params.id)
+    .then(exercise => {
+        exercise.username = req.body.username;
+        exercise.description = req.body.description;
+        exercise.duration = req.body.duration;
+        exercise.date = req.body.date;
+        exercise.save()}
+        )
+        .then(() => res.json('Exercise Updated'))
+        .catch(err => res.status(400).json('Error'+err))
+    .catch(err=>res.status(400).json('Error')+err)})
+
+
+
+module.exports = router;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -7,7 +7,7 @@ router.route('/').get((req,res)=>{
     .catch(err => res.status(400).json('Error'+err));
 });
 
-router.route('/add').get((req,res)=>{
+router.route('/add').post((req,res)=>{
     const username = req.body.username;
     const newUser = new User({username});
     newUser.save()
